340e1520361a64b41761cfb4fd1e7d14b8dc9b3e
nalim84r <105993657+nalim84r@users.noreply.github.com>
2022-06-06 20:11:47 +0200

    Add files via upload

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,16 +1,40 @@
 #Einfacher Taschenrechner
 #Benutzeroberfläche
-#Multiplizieren
+#Vier Grundrechenarten
 #Manuelle Werte Eingabe
 #Ausgabe
 
 import tkinter
+
+def addieren():
+    try:
+        zahl1 = int(etEingabe1.get())
+        zahl2 = int(etEingabe2.get())
+        lbAusgabe["text"] = zahl1 + zahl2
+    except:
+        lbAusgabe["text"] = "Keine Zahlen"
+
+def subtrahieren():
+    try:
+        zahl1 = int(etEingabe1.get())
+        zahl2 = int(etEingabe2.get())
+        lbAusgabe["text"] = zahl1 - zahl2
+    except:
+        lbAusgabe["text"] = "Keine Zahlen"
 
 def multiplizieren():
     try:
         zahl1 = int(etEingabe1.get())
         zahl2 = int(etEingabe2.get())
         lbAusgabe["text"] = zahl1 * zahl2
+    except:
+        lbAusgabe["text"] = "Keine Zahlen"
+
+def dividieren():
+    try:
+        zahl1 = int(etEingabe1.get())
+        zahl2 = int(etEingabe2.get())
+        lbAusgabe["text"] = zahl1 / zahl2
     except:
         lbAusgabe["text"] = "Keine Zahlen"  
 
@@ -20,22 +44,33 @@ def ende():
 
 
 fenster = tkinter.Tk()
-fenster.title("Multiplizieren")
+fenster.title("+-*/")
 fenster.resizable(0,0)
 
 lbEingabe = tkinter.Label(fenster, text="Ihre Eingabe:")
-lbEingabe.grid(row=0, column=0, sticky="w", padx=5, pady=5)
+lbEingabe.grid(row=0, column=0, padx=5, pady=5)
 
-etEingabe1 = tkinter.Entry(fenster)
-etEingabe1.grid(row=1, column=0,padx=5, pady=5)
+etEingabe1 = tkinter.Entry(fenster, width=13)
+etEingabe1.grid(row=0, column=1, padx=5, pady=5)
 
-etEingabe2 = tkinter.Entry(fenster)
-etEingabe2.grid(row=2, column=0,padx=5, pady=5)
+etEingabe2 = tkinter.Entry(fenster, width=13)
+etEingabe2.grid(row=0, column=2, padx=5, pady=5)
+
+buAdd = tkinter.Button(fenster, text="addieren", command=addieren,width=10)
+buAdd.grid(row=1,column=1,sticky="w", padx=5, pady=5)
+
+buSub = tkinter.Button(fenster, text="subtrahieren", command=subtrahieren,width=10)
+buSub.grid(row=1, column=2, sticky="w",padx=5, pady=5)
 
 buMulti = tkinter.Button(fenster, text="multiplizieren", command=multiplizieren,width=10)
-buMulti.grid(row=3, column=0,sticky="w",padx=5, pady=5)
+buMulti.grid(row=2, column=1, sticky="w", padx=5, pady=5)
 
-lbAusgabe = tkinter.Label(fenster,text="... wir!")
+buDiv = tkinter.Button(fenster, text="dividieren", command=dividieren,width=10)
+buDiv.grid(row=2, column=2, sticky="w", padx=5, pady=5)
+
+
+
+lbAusgabe = tkinter.Label(fenster,text="rechnen wir!")
 lbAusgabe.grid(row=4, column=0,sticky="w",padx=5, pady=5)
 
 buEnde = tkinter.Button(fenster, text="Ende", command=ende,width=10)
